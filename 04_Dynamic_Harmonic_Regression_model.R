ff085ff2c106476424e0ac5cab81754e77c64f61
Felipe S. Mezzarana <spinelli.mezzarana@gmail.com>
2022-06-16 20:26:45 -0300

    Finalizing model 04 and small corrections

--- 04_Dynamic_Harmonic_Regression_model.R
+++ 04_Dynamic_Harmonic_Regression_model.R
@@ -4,17 +4,15 @@ library(forecast)
 #importing data
 min_temp <- read.csv("C:/Users/spine/PYTHON/daily_mim_temp/daily-minimum-temperatures-in-me-treated_v2.csv")
 tail(min_temp)
-temperatures = min_temp[c("daily_min_temp")]
+temperatures <- min_temp[c("daily_min_temp")]
 
-
-## Create a daily Date object - helps my work on dates
+# Create a daily Date object - helps my work on dates
 inds <- seq(as.Date("1981-01-02"), as.Date("1990-12-31"), by = "day")
 
-## Create a time series object
+# Create a time series object
 myts <- ts(temperatures,    start = c(1981, as.numeric(format(inds[1], "%j"))),frequency = 365)
 
 bestfit <- list(aicc=Inf)
-
 for (i in seq(12)) {
   fit <- auto.arima(myts, xreg = fourier(myts, K = i))
   aiic <- fit[["aicc"]]
@@ -24,24 +22,32 @@ for (i in seq(12)) {
     bestfit <- fit
     bestK <- i}
 }
+
 #checking our model 
 print(bestK)
 print(bestfit)
+final_fit[["aicc"]]
 checkresiduals(bestfit, lag=15)
 
-#testing (delete later)
-final_fit <- Arima(myts,order=c(3,1,1), xreg = fourier(myts, K = 2))
-final_fit[["aicc"]]
-fcast <- forecast(final_fit, xreg = fourier(myts, K = 2))
+#The auto.arima found an optimized model with order=c(3,1,1) and K = 2
 
-#validation loop  - under development
+#validation loop, lets train the model only with the data collected before the forecast date. 
+#That is, we will train a model for each forecast, dropping the previous data.
 train_ts <- myts
-for (i in seq(3)){
+fcast_list <- list()
+date_list <-list()
+for (i in seq(3250)){
   train_ts <- head(train_ts,-1)  
-  tail(train_ts)
+  final_fit <- Arima(train_ts,order=c(3,1,1), xreg = fourier(train_ts, K = 2))
+  fcast <- forecast(final_fit, h=1, xreg = fourier(train_ts, K = 2))
+  fcast_df <- data.frame(fcast)
+  point_fcast <- fcast_df[1,1]
+  fcast_list<- append(fcast_list,point_fcast)
+  date_list<-append(date_list,min_temp[3653-i,1])
 }
 
-
-
+#create and save DataFrame
+fcast_df <- do.call(rbind, Map(data.frame, date=date_list, forecast=fcast_list))
+write.csv(fcast_df, "C:\\Users\\spine\\PYTHON\\forecast_models\\Forecast Files\\dynamic_regression_forecast.csv", row.names=FALSE)
 
 
